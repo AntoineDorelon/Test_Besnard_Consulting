0854764eec5ae2179fa973e73312c3cfc0abe3ec
Antoine Dorelon <antoinedorelon@gmail.com>
2021-05-21 16:39:16 +0200

    adding limit of 4 values and 12 principles in the backend

--- backend_DRF/api/views.py
+++ backend_DRF/api/views.py
@@ -1,14 +1,34 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status, mixins
 from .models import AgileValues, AgilePrinciples
 from .serializers import ValuesSerializer, PrinciplesSerializer
+from rest_framework.response import Response
 
 
 class ValuesViewset(viewsets.ModelViewSet):
     queryset = AgileValues.objects.all()
     serializer_class = ValuesSerializer
 
+    def create(self, request, *args, **kwargs):
+        number_of_values = AgileValues.objects.all().count()
+        # print(f'there are already {number_of_values} values in database')
+        if number_of_values < 4:
+            return super().create(request, *args, **kwargs)
+        else:
+            response = {'message': 'You cant create more than 4 values'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
 
 class PrinciplesViewset(viewsets.ModelViewSet):
     queryset = AgilePrinciples.objects.all()
     serializer_class = PrinciplesSerializer
+
+    def create(self, request, *args, **kwargs):
+        numbers_of_principles = AgilePrinciples.objects.all().count()
+        if numbers_of_principles < 12:
+            return super().create(request, *args, **kwargs)
+        else:
+            response = {'message': 'You cant create more than 12 values'}
+            return Response(response, status.HTTP_400_BAD_REQUEST)
+
+
 
